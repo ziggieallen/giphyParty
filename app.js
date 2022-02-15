0e98e0dcd9e415a9b87d316e40c89eff7624535a
ziggieallen <93275742+ziggieallen@users.noreply.github.com>
2022-02-15 07:50:15 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('#searchform');
+form.addEventListener('submit', async function(e) {
+    e.preventDefault();
+    const urls = [];
+    const res = await axios.get("https://api.giphy.com/v1/gifs/search?", {
+        params: {
+            q: search.value,
+            api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym'
+        }
+    });
+    for (let url of res.data.data) {
+        urls.push(res.data.data[0].images.original.url)
+    }
+    randomGif = urls[Math.floor(Math.random() * urls.length)]
+    //supposed to randomly generate gifs from list of urls but is coming up with
+    // the same gifs
+
+    console.log(randomGif)
+    nextGif = document.createElement('img')
+    nextGif.setAttribute('src', `${randomGif}`)
+    nextGif.classList.add('gifs')
+    document.body.appendChild(nextGif)
+    search.value = ''
+    // I'm not actually sure what the purpose of the above line is
+    // but I keep seeing it when writing programs that are apending something
+});
+
+const removeBtn = document.querySelector('#remove')
+removeBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    $('.gifs').remove()
+})
+
